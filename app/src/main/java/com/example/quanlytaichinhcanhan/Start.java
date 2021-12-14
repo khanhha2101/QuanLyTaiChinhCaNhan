1aad31236f6755ff99345678819e91526efb82b4
khanhha <khanhhadsdl@gmail.com>
2021-12-14 22:35:17 +0700

    start và đăng nhập

---
+++ app/src/main/java/com/example/quanlytaichinhcanhan/Start.java
@@ -0,0 +1,29 @@
+package com.example.quanlytaichinhcanhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Start extends AppCompatActivity {
+
+    Button btn_start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        btn_start = findViewById(R.id.btn_start);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Start.this, Start.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
