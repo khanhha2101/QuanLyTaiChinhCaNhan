566b258ca68e93217dcf2af0660be053204da808
TLinh <nguyenthuylinh08042001>
2021-12-15 15:46:30 +0700

    Linh up Trang chủ 1

---
+++ app/src/main/java/com/example/quanlytaichinhcanhan/TrangChu1.java
@@ -0,0 +1,14 @@
+package com.example.quanlytaichinhcanhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrangChu1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chu1);
+    }
+}
