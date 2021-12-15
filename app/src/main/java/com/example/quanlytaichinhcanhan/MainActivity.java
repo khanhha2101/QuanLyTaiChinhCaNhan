0561b3a74eea408f51910ad6599c2756b823c113
khanhha <khanhhadsdl@gmail.com>
2021-12-15 11:42:29 +0700

    start và đăng nhập

--- app/src/main/java/com/example/quanlytaichinhcanhan/MainActivity.java
+++ app/src/main/java/com/example/quanlytaichinhcanhan/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.quanlytaichinhcanhan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void bottomNavigation() {
+        FloatingActionButton btn_add = findViewById(R.id.btn_add);
+        LinearLayout btn_home = findViewById(R.id.btn_home);
+        LinearLayout btn_thongke = findViewById(R.id.btn_thongke);
+        LinearLayout btn_lichsu = findViewById(R.id.btn_lichsu);
+        LinearLayout btn_caidat = findViewById(R.id.btn_caidat);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_thongke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_lichsu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_caidat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
