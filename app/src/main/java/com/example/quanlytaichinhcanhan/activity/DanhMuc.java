861aef3a87783d9c839d1ba2d5c241e68b68f468
khanhha <khanhhadsdl@gmail.com>
2021-12-24 13:55:05 +0700

    danh mục

--- app/src/main/java/com/example/quanlytaichinhcanhan/activity/DanhMuc.java
+++ app/src/main/java/com/example/quanlytaichinhcanhan/activity/DanhMuc.java
@@ -1,15 +1,19 @@
 package com.example.quanlytaichinhcanhan.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.quanlytaichinhcanhan.R;
 import com.example.quanlytaichinhcanhan.adapter.DanhmucAdapter;
+import com.example.quanlytaichinhcanhan.animation.TranslateAnimationUtil;
 import com.example.quanlytaichinhcanhan.api.ApiService;
 import com.example.quanlytaichinhcanhan.model.danhmuc;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -20,7 +24,7 @@ import retrofit2.Response;
 public class DanhMuc extends AppCompatActivity {
 
     RecyclerView rcv_dm;
-
+    FloatingActionButton btn_add_danhmuc;
     DanhmucAdapter danhmucAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,19 @@ public class DanhMuc extends AppCompatActivity {
         rcv_dm = findViewById(R.id.rcv_danhmuc);
 
         GetDataRecyclerView();
+
+        rcv_dm.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if(dy > 0) {
+                    btn_add_danhmuc.setVisibility(View.GONE);
+                } else {
+                    btn_add_danhmuc.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+//        rcv_dm.setOnTouchListener(new TranslateAnimationUtil(this, btn_add_danhmuc));
 
     }
 
@@ -55,5 +72,6 @@ public class DanhMuc extends AppCompatActivity {
         danhmucAdapter.setData(list);
         rcv_dm.setLayoutManager(linearLayoutManager);
         rcv_dm.setAdapter(danhmucAdapter);
+        btn_add_danhmuc = findViewById(R.id.btn_add_danhmuc);
     }
 }
